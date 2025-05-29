b6fa25a71c19d31b2949b05c4ab48c3ae3e50ba2
Sage Wall <swall@esri.com>
2025-05-29 11:45:52 -0600

    feat: add save and saveAs

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import Color from "@arcgis/core/Color";
-import Map from "@arcgis/core/Map";
 import SimpleFillSymbol from "@arcgis/core/Symbols/SimpleFillSymbol";
 import SimpleLineSymbol from "@arcgis/core/Symbols/SimpleLineSymbol";
 import SimpleMarkerSymbol from "@arcgis/core/Symbols/SimpleMarkerSymbol";
+import WebMap from "@arcgis/core/WebMap";
+import esriConfig from "@arcgis/core/config";
 import VideoLayer from "@arcgis/core/layers/VideoLayer";
+import PortalItem from "@arcgis/core/portal/PortalItem";
 import "@arcgis/map-components/components/arcgis-map";
 import "@arcgis/map-components/components/arcgis-placement";
 import "@arcgis/map-components/components/arcgis-video-player";
 import "@esri/calcite-components/components/calcite-block";
+import "@esri/calcite-components/components/calcite-button";
 import "@esri/calcite-components/components/calcite-label";
 import "@esri/calcite-components/components/calcite-shell";
 import "@esri/calcite-components/components/calcite-slider";
 import "@esri/calcite-components/components/calcite-switch";
 import "./style.css";
+
+esriConfig.portalUrl = "https://devtesting.mapsdevext.arcgis.com/";
 
 const frameEffectBrightnessSlider = document.querySelector(
   "#frame-effect-brightness-slider"
@@ -32,6 +37,12 @@ const frameOpacitySlider = document.querySelector(
 const opacitySlider = document.querySelector(
   "#opacity-slider"
 )! as HTMLCalciteSliderElement;
+const saveButton = document.querySelector(
+  "#save-button"
+)! as HTMLCalciteButtonElement;
+const saveAsButton = document.querySelector(
+  "#save-as-button"
+)! as HTMLCalciteButtonElement;
 const viewElement = document.querySelector("arcgis-map")!;
 const videoPlayerElement = document.querySelector("arcgis-video-player")!;
 
@@ -118,12 +129,12 @@ const videoLayer = new VideoLayer({
   visible: true,
 });
 
-const map = new Map({
+const webMap = new WebMap({
   basemap: "topo-vector",
   layers: [videoLayer],
 });
 
-viewElement.map = map;
+viewElement.map = webMap;
 
 load();
 
@@ -163,6 +174,24 @@ frameEffectInvertSwitch.addEventListener("calciteSwitchChange", () => {
 
 frameEffectSaturateSlider.addEventListener("calciteSliderInput", () => {
   updateFrameEffect();
+});
+
+saveButton.addEventListener("click", async () => {
+  console.log("save button clicked");
+  const result = await webMap.save();
+  console.log("Save result:", result.id);
+});
+
+saveAsButton.addEventListener("click", async () => {
+  await webMap.loadAll();
+  webMap.updateFrom(viewElement.view);
+  console.log("save as button clicked");
+  const result = await webMap.saveAs(
+    new PortalItem({ title: "My Video Layer Web Map" })
+  );
+  console.log("Save As result:", result.id);
+  webMap.portalItem = result;
+  saveButton.disabled = false;
 });
 
 async function load() {
