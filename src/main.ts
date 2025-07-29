e69aedae225ac223ada363ba6c7fe0b0f1a53faa
Sage Wall <swall@esri.com>
2025-07-29 12:07:35 -0600

    feat: add layers select

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,28 @@ import SimpleLineSymbol from "@arcgis/core/Symbols/SimpleLineSymbol";
 import SimpleMarkerSymbol from "@arcgis/core/Symbols/SimpleMarkerSymbol";
 import WebMap from "@arcgis/core/WebMap";
 import esriConfig from "@arcgis/core/config";
+import Layer from "@arcgis/core/layers/Layer";
 import VideoLayer from "@arcgis/core/layers/VideoLayer";
 import PortalItem from "@arcgis/core/portal/PortalItem";
+import esriRequest from "@arcgis/core/request";
 import "@arcgis/map-components/components/arcgis-map";
 import "@arcgis/map-components/components/arcgis-placement";
 import "@arcgis/map-components/components/arcgis-video-player";
 import "@esri/calcite-components/components/calcite-block";
 import "@esri/calcite-components/components/calcite-button";
 import "@esri/calcite-components/components/calcite-label";
+import "@esri/calcite-components/components/calcite-option";
+import "@esri/calcite-components/components/calcite-select";
 import "@esri/calcite-components/components/calcite-shell";
 import "@esri/calcite-components/components/calcite-slider";
 import "@esri/calcite-components/components/calcite-switch";
 import { airplanePath } from "./airplane-path";
 import "./style.css";
+
+const state = {
+  videoLayer: new VideoLayer(),
+  webMap: new WebMap(),
+};
 
 esriConfig.portalUrl = "https://devtesting.mapsdevext.arcgis.com/";
 
@@ -47,126 +56,36 @@ const saveButton = document.querySelector(
 const saveAsButton = document.querySelector(
   "#save-as-button"
 )! as HTMLCalciteButtonElement;
+const testingPropertiesSwitch = document.querySelector(
+  "#testing-properties-switch"
+)! as HTMLCalciteSwitchElement;
 const viewElement = document.querySelector("arcgis-map")!;
+const videoLayerSelect = document.querySelector(
+  "#video-layer-select"
+)! as HTMLCalciteSelectElement;
 const videoPlayerElement = document.querySelector("arcgis-video-player")!;
-
-const videoLayer = new VideoLayer({
-  autoplay: true,
-  // blendMode: "vivid-light",
-  effect: "brightness(500%) hue-rotate(270deg) contrast(200%)",
-  // frameEffect: "invert()",
-  frameOpacity: 0.1,
-  frameCenterSymbol: new SimpleMarkerSymbol({
-    angle: 0,
-    color: new Color([0, 0, 255, 1]),
-    outline: new SimpleLineSymbol({
-      cap: "round",
-      color: new Color([0, 255, 0, 1]),
-      join: "round",
-      miterLimit: 1,
-      style: "short-dash-dot-dot",
-      width: 2,
-    }),
-    size: 12,
-    style: "triangle",
-    xoffset: 0,
-    yoffset: 0,
-  }),
-  frameOutlineSymbol: new SimpleFillSymbol({
-    color: new Color([255, 0, 0, 1]),
-    outline: new SimpleLineSymbol({
-      cap: "round",
-      color: new Color([0, 255, 0, 1]),
-      join: "round",
-      miterLimit: 1,
-      style: "dot",
-      width: 12,
-    }),
-    style: "vertical",
-  }),
-  muted: true,
-  opacity: 0.5,
-  sensorSightLineSymbol: new SimpleLineSymbol({
-    cap: "round",
-    color: new Color([255, 255, 0, 1]),
-    join: "round",
-    miterLimit: 1,
-    style: "dash-dot",
-    width: 3,
-  }),
-  sensorSymbol: new SimpleMarkerSymbol({
-    angle: 90,
-    color: new Color([0, 0, 255, 1]),
-    outline: new SimpleLineSymbol({
-      cap: "round",
-      color: new Color([0, 255, 0, 1]),
-      join: "round",
-      miterLimit: 1,
-      style: "solid",
-      width: 3,
-    }),
-    path: airplanePath,
-    size: 24,
-    xoffset: 0,
-    yoffset: 0,
-  }),
-  // @ts-ignore
-  sensorSymbolOrientation: {
-    source: "platformHeading", // "cameraAzimuth" or "platformHeading"
-    symbolOffset: 0,
-  },
-  sensorTrailSymbol: new SimpleLineSymbol({
-    cap: "round",
-    color: new Color([255, 0, 0, 1]),
-    join: "round",
-    miterLimit: 1,
-    style: "dash",
-    width: 12,
-  }),
-  start: 6,
-  telemetryDisplay: {
-    frame: true,
-    frameCenter: true,
-    frameOutline: true,
-    lineOfSight: true,
-    sensorLocation: true,
-    sensorTrail: true,
-  },
-  title: "The best video layer in the world",
-  url: "https://dev000276.esri.com/video/rest/services/cheyenne1/VideoServer/0",
-  visible: true,
-});
-
-const webMap = new WebMap({
-  portalItem: {
-    id: "e61993e233b34bf4a12441b61129e15a",
-  },
-});
-webMap.layers.add(videoLayer);
-
-viewElement.map = webMap;
 
 init();
 
 opacitySlider.addEventListener("calciteSliderInput", () => {
   const value = opacitySlider.value;
   if (typeof value === "number") {
-    videoLayer.opacity = value / 100;
+    state.videoLayer.opacity = value / 100;
   } else if (Array.isArray(value) && typeof value[0] === "number") {
-    videoLayer.opacity = value[0] / 100;
+    state.videoLayer.opacity = value[0] / 100;
   } else {
-    videoLayer.opacity = 1;
+    state.videoLayer.opacity = 1;
   }
 });
 
 frameOpacitySlider.addEventListener("calciteSliderInput", () => {
   const value = frameOpacitySlider.value;
   if (typeof value === "number") {
-    videoLayer.frameOpacity = value / 100;
+    state.videoLayer.frameOpacity = value / 100;
   } else if (Array.isArray(value) && typeof value[0] === "number") {
-    videoLayer.frameOpacity = value[0] / 100;
+    state.videoLayer.frameOpacity = value[0] / 100;
   } else {
-    videoLayer.frameOpacity = 1;
+    state.videoLayer.frameOpacity = 1;
   }
 });
 
@@ -188,49 +107,200 @@ frameEffectSaturateSlider.addEventListener("calciteSliderInput", () => {
 
 loadButton.addEventListener("click", async () => {
   console.log("load button clicked");
-  viewElement.itemId = webMap.portalItem?.id;
+  viewElement.itemId = state.webMap.portalItem?.id;
 });
 
 saveButton.addEventListener("click", async () => {
   console.log("save button clicked");
-  const result = await webMap.save();
+  const result = await state.webMap.save();
   console.log("Save result:", result.id, result);
 });
 
 saveAsButton.addEventListener("click", async () => {
-  await webMap.loadAll();
-  webMap.updateFrom(viewElement.view);
+  await state.webMap.loadAll();
+  state.webMap.updateFrom(viewElement.view);
   console.log("save as button clicked");
-  const result = await webMap.saveAs(
+  const result = await state.webMap.saveAs(
     new PortalItem({ title: "My Video Layer Web Map" })
   );
   console.log("Save As result:", result.id, result);
-  webMap.portalItem = result;
+  state.webMap.portalItem = result;
   saveButton.disabled = false;
   loadButton.disabled = false;
 });
 
+testingPropertiesSwitch.addEventListener("calciteSwitchChange", () => {
+  updateVideoLayer(videoLayerSelect.value);
+});
+
+videoLayerSelect.addEventListener("calciteSelectChange", async (event) => {
+  const selectedOption = event.target as HTMLCalciteSelectElement;
+  const selectedValue = selectedOption.value;
+  updateVideoLayer(selectedValue);
+});
+
+async function getPortalItems(portalUrl: string) {
+  const url = portalUrl + "/sharing/rest/search";
+
+  const options = {
+    query: {
+      q: 'type:"Video Service"',
+      f: "json",
+    },
+  };
+
+  const response = await esriRequest(url, options);
+  const portalItems = response.data.results;
+  return portalItems;
+}
+
 async function init() {
+  const portalItems = await getPortalItems(
+    "https://video-portal.idt.geocloud.com/portal"
+  );
+
+  console.log("Portal Items:", portalItems);
+
+  portalItems.forEach((item: PortalItem) => {
+    const option = document.createElement("calcite-option");
+    option.value = item.id;
+    option.label = item.title ?? "";
+    videoLayerSelect.appendChild(option);
+  });
+
+  updateVideoLayer(portalItems[0].id);
+
+  state.webMap = new WebMap({
+    portalItem: {
+      id: "e61993e233b34bf4a12441b61129e15a",
+    },
+  });
+  state.webMap.layers.add(state.videoLayer);
+
+  viewElement.map = state.webMap;
+
   await viewElement.viewOnReady();
   console.log("the view is ready");
 
-  if (videoLayer.loaded) {
+  if (state.videoLayer.loaded) {
     console.log("the layer is loaded");
-    videoPlayerElement.layer = videoLayer;
+    videoPlayerElement.layer = state.videoLayer;
   } else {
-    await videoLayer.load();
-    videoPlayerElement.layer = videoLayer;
+    await state.videoLayer.load();
+    videoPlayerElement.layer = state.videoLayer;
   }
-  await viewElement.whenLayerView(videoLayer);
+  await viewElement.whenLayerView(state.videoLayer);
   console.log("the layerview is created");
-  viewElement.goTo(videoLayer.fullExtent);
+  viewElement.goTo(state.videoLayer.fullExtent);
 
   updateFrameEffect();
 }
 
 function updateFrameEffect() {
-  videoLayer.frameEffect = `brightness(${frameEffectBrightnessSlider.value}%) contrast(${frameEffectContrastSlider.value}%) saturate(${frameEffectSaturateSlider.value}%)`;
+  state.videoLayer.frameEffect = `brightness(${frameEffectBrightnessSlider.value}%) contrast(${frameEffectContrastSlider.value}%) saturate(${frameEffectSaturateSlider.value}%)`;
   if (frameEffectInvertSwitch.checked) {
-    videoLayer.frameEffect += " invert()";
+    state.videoLayer.frameEffect += " invert()";
   }
+}
+
+async function updateVideoLayer(id: string) {
+  const layer = await Layer.fromPortalItem({
+    portalItem: new PortalItem({
+      id,
+      portal: { url: "https://video-portal.idt.geocloud.com/portal" },
+    }),
+  });
+  state.webMap.layers.remove(state.videoLayer);
+  state.videoLayer = layer as VideoLayer;
+  state.webMap.layers.add(state.videoLayer);
+  videoPlayerElement.layer = state.videoLayer;
+  if (testingPropertiesSwitch.checked) {
+    addTestingProperties();
+  }
+}
+
+function addTestingProperties() {
+  state.videoLayer.autoplay = true;
+  state.videoLayer.blendMode = "vivid-light";
+  state.videoLayer.effect =
+    "brightness(500%) hue-rotate(270deg) contrast(200%)";
+  state.videoLayer.frameEffect = "invert()";
+  state.videoLayer.frameOpacity = 0.1;
+  state.videoLayer.frameCenterSymbol = new SimpleMarkerSymbol({
+    angle: 0,
+    color: new Color([0, 0, 255, 1]),
+    outline: new SimpleLineSymbol({
+      cap: "round",
+      color: new Color([0, 255, 0, 1]),
+      join: "round",
+      miterLimit: 1,
+      style: "short-dash-dot-dot",
+      width: 2,
+    }),
+    size: 12,
+    style: "triangle",
+    xoffset: 0,
+    yoffset: 0,
+  });
+  state.videoLayer.frameOutlineSymbol = new SimpleFillSymbol({
+    color: new Color([255, 0, 0, 1]),
+    outline: new SimpleLineSymbol({
+      cap: "round",
+      color: new Color([0, 255, 0, 1]),
+      join: "round",
+      miterLimit: 1,
+      style: "dot",
+      width: 12,
+    }),
+    style: "vertical",
+  });
+  state.videoLayer.muted = true;
+  state.videoLayer.opacity = 0.5;
+  state.videoLayer.sensorSightLineSymbol = new SimpleLineSymbol({
+    cap: "round",
+    color: new Color([255, 255, 0, 1]),
+    join: "round",
+    miterLimit: 1,
+    style: "dash-dot",
+    width: 3,
+  });
+  state.videoLayer.sensorSymbol = new SimpleMarkerSymbol({
+    angle: 90,
+    color: new Color([0, 0, 255, 1]),
+    outline: new SimpleLineSymbol({
+      cap: "round",
+      color: new Color([0, 255, 0, 1]),
+      join: "round",
+      miterLimit: 1,
+      style: "solid",
+      width: 3,
+    }),
+    path: airplanePath,
+    size: 24,
+    xoffset: 0,
+    yoffset: 0,
+  });
+  // @ts-ignore
+  state.videoLayer.sensorSymbolOrientation = {
+    source: "platformHeading", // "cameraAzimuth" or "platformHeading"
+    symbolOffset: 0,
+  };
+  state.videoLayer.sensorTrailSymbol = new SimpleLineSymbol({
+    cap: "round",
+    color: new Color([255, 0, 0, 1]),
+    join: "round",
+    miterLimit: 1,
+    style: "dash",
+    width: 12,
+  });
+  state.videoLayer.start = 6;
+  state.videoLayer.telemetryDisplay = {
+    frame: true,
+    frameCenter: true,
+    frameOutline: true,
+    lineOfSight: true,
+    sensorLocation: true,
+    sensorTrail: true,
+  };
+  state.videoLayer.visible = true;
 }
